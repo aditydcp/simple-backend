a11b99889f1556a82b0669c1a8b04a4c42c1d876
Aditya Cahyo <aditydcp@gmail.com>
2023-02-10 18:54:44 +0700

    add file upload handler

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
+const multiparty = require('multiparty');
 const bodyParser = require('body-parser');
+var format = require('util').format;
 
 // Curb Cores Error by adding a header here
 app.use((req, res, next) => {
@@ -20,6 +22,9 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// connect-form configuration
+
+
 // ENDPOINTS
 
 app.get("/", (request, response, next) => {
@@ -33,14 +38,56 @@ app.post("/message", (request, response) => {
         response.status(204).send({
             message: "Null content arrived",
             content: request.body.content || null,
-        })
+        });
     }
     else {
         response.status(202).send({
             message: "Content arrived",
             content: request.body.content,
-        })
+        });
     }
+})
+
+// File Upload Enpoint
+app.post("/file", (request, response, next) => {
+    var form = new multiparty.Form();
+    var image;
+
+    form.on('error', next);
+    form.on('close', function(){
+        // response.send(format(
+        //     '\nuploaded %s (%d Kb)',
+        //     image.filename,
+        //     image.size / 1024 | 0
+        // ));
+
+        response.status(200).send({
+            message: "File uploaded",
+            filename: image.filename,
+            sizeInKb: `${image.size / 1024 | 0} Kb`
+        });
+    })
+
+    // listen on part event for image file
+    form.on('part', function(part){
+        if (!part.filename) return;
+        if (part.name !== 'image') return part.resume();
+        image = {};
+        image.filename = part.filename;
+        image.size = 0;
+        part.on('data', function(buf){
+            image.size += buf.length;
+        });
+    });
+
+    // form.on('progress', function(){
+    //     response.status(102).send({
+    //         message: "Uploading file"
+    //     });
+    // })
+
+    // parse the form
+    form.parse(request);
 })
 
 module.exports = app;
